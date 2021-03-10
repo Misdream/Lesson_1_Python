5a78187b5eb85058933792560f267688bd1ed3c6
misdream <misdream@yandex.ru>
2021-03-10 23:05:18 +0300

    Initial commit

---
+++ Lesson1_DZ.py
@@ -0,0 +1,27 @@
+# Задание 2
+# seconds = int(input('Введите количество секунд: '))
+# hours = seconds // 3600
+# minutes = (seconds - hours*3600) // 60
+# seconds = seconds - hours*3600 - minutes*60
+# print(f'Часы:минуты:секунды {hours:02}:{minutes:02}:{seconds:02}')
+#
+#Задание 3
+# number = int(input('Введите положительное число от 1 до 9: '))
+# while number < 0 or number > 9:
+#     number = int(input('Введите положительное число от 1 до 9: '))
+#
+# print('n =', number)
+# print(number + number *10 + number + number*100 + number*10 + number)
+
+#Задание 5
+# number = int(input('Введите целое положительное число: '))
+# max_digit = 0
+# while number > 0:
+#     last_digit = number % 10
+#     if last_digit > max_digit:
+#         max_digit = last_digit
+#     number = number // 10
+#
+# print('Максимальная цифра в числе', max_digit)
+
+
